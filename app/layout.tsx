9bcbb580c71d951b9b577d7ed5b86640c99a1c2c
Abdullah Alshawadfy <abdullahalshawadfy410@gmail.com>
2026-05-20 23:18:58 +0300

    Load Microsoft Clarity before hydration (beforeInteractive)

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,15 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <Script id="microsoft-clarity" strategy="beforeInteractive" src="https://www.clarity.ms/tag/wu7dz0nxre" />
+      </head>
       <body>
-        <Script id="microsoft-clarity" strategy="afterInteractive">
-          {`(function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "wu7dz0nxre");`}
-        </Script>
-
         {children}
       </body>
     </html>
